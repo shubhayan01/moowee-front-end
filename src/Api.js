41ea65f60711b04203e5cdb371c804beed1482f2
shubhayan01 <shubhayan.work@gmail.com>
2026-01-03 21:27:09 +0530

    Point API baseURL to deployed backend

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://moowee-back-end.onrender.com/api",
   withCredentials: true,
 });
 
